58904dbe0ead1129c81d0aa633a7932125b4ccff
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-01-17 12:16:27 +0200

    chnage the  intro

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -10,8 +10,8 @@ export const Intro = () => (
 		<Header />
 		<IntroWrapper as={Container}>
 			<Details>
-				<h1>Hi There!</h1>
-				<h4>I’m Yazan  and I’m Full Stack Python Developer!</h4>
+				<h1>Yazan Al Monshed</h1>
+				<h4> Full Stack Python / Django Developer</h4>
 				<Button as={AnchorLink} href="#contact">
 					Contact	Me
 				</Button>
