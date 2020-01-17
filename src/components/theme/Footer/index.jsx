4d22fa5f2278b755855b653e6c8291eef0a25b68
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-01-18 01:06:45 +0200

    remove fontawsome and add svg images

--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Container } from 'components/common'
+// import twitter from '../../../assets/icons/twitter.svg'
 import { Wrapper, Flex, Links, Details } from './styles'
-// import social from './social.json'
+import social from './social.json'
 
 export const Footer = () => (
 	<Wrapper>
@@ -14,23 +15,21 @@ export const Footer = () => (
 			</Details>
 			
 			<Links>
-				{/* {social.map(({ id, name, link, icon }) => (
-					<a
+				{social.map(({ id, name, link, icon,title}) => (
+					<a   
 						key={id}
 						href={link}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label={`follow me on ${name}`}
+						title={title}
 					>
-						<img width="24" src={icon} alt={name} />
+						<img width="30" src={icon} alt={name} />
 					</a>
-				))} */}
-				<a href="https://github.com/YazanALMonshed" target="_blank"><i style={{fontSize: '1.8rem', padding: '.4rem'}} className="fab fa-github"></i></a>
-				<a href="https://www.facebook.com/profile.php?id=100011284960911" target="_blank"><i style={{fontSize: '1.8em', padding: '.4rem'}} className="fab fa-facebook-f"></i></a>
-				<a href="https://twitter.com/YAZANMONSHED?lang=ar" target="_blank"><i style={{fontSize: '1.8rem', padding: '.4rem'}} className="fab fa-twitter"></i></a>
-				<a href="https://www.linkedin.com/in/yazan-al-monshed-52b65215a/" target="_blank"><i style={{fontSize: '1.8rem', padding: '.4rem'}} className="fab fa-linkedin-in"></i></a>
-				<a href="https://www.goodreads.com/user/show/70631180-yazan-al-monshed" target="_blank"><i style={{fontSize: '1.8rem', padding: '.4rem'}} className="fab fa-goodreads-g"></i></a>
+				))}
 			</Links>
 		</Flex>
 	</Wrapper>
 )
+
+
