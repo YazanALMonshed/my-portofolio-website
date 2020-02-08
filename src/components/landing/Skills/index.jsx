9ba848592c5aae1a57907c5769323c6cb8c0bcbb
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-09 00:46:20 +0200

    fixed section skills

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Container, Button } from 'components/common'
-import dev from 'assets/illustrations/about.svg'
+import dev from 'assets/illustrations/about-theme-2.svg'
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
 
 export const Skills = () => (
