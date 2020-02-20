dfdc2f92e5c6850afaa3608ad026680451884b11
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-20 19:08:39 +0200

    add simple style to 404 page

---
+++ src/pages/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 2.2rem
+
+`
+export const ContainerCenter = styled.div`
+
+    text-aign: center;
+    font-weight: bold;
+    color: #F00;
+
+`
