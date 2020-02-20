0c765ba3863f0209b3959b457843416dfda345fb
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-20 19:09:00 +0200

    edit style page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
-
+import {Container, ContainerCenter} from './styles.js'
 export default () => (
   <Layout>
-
     <SEO title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  
+    <Container>
+      <ContainerCenter>
+      <h1>NOT FOUND PAGE</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+     </ContainerCenter>
+    </Container>
   </Layout>
 );
