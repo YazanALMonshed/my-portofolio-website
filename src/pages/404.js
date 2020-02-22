65c78b26b4b758fb7b23271c652b9d11a404b3fc
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-02-22 19:11:55 +0200

    fixed errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
-import {Container, ContainerCenter} from './styles.js'
 export default () => (
   <Layout>
     <SEO title="404: Not found" location="/404" />
-    <Container>
-      <ContainerCenter>
+  
       <h1>NOT FOUND PAGE</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-     </ContainerCenter>
-    </Container>
+    
   </Layout>
 );
