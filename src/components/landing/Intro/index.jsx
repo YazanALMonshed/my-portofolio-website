c0bb0e6334c1816117caa4b3efa92a27c38b5b82
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-06-26 14:52:52 +0300

    Edit Intro Section

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -11,7 +11,7 @@ export const Intro = () => (
 		<IntroWrapper as={Container}>
 			<Details>
 				<h1>Yazan Al Monshed</h1>
-				<h4>Software Developer. GNU Linux User. Contributor Free & Open Source Software</h4>
+				<h4>Software Engineer. GNU Linux User. Contributor Free & Open Source Software</h4>
 				<Button as={AnchorLink} href="#contact">
 					Contact	Me
 				</Button>
