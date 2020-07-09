d02ea0dc45a02411417deb30df1a391090fb711c
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-07-09 19:25:14 +0300

    edit Skills section

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -13,7 +13,7 @@ export const Skills = () => (
       <Details>
         <h1>About Me</h1>
         <p>
-        I’ve long been interested in software development and design and always enjoy learning about new and exciting technologies. my experience in Python, Frameworks Django & Flask. Golang & JavaScript libraries like ReactJs, & Bash. I love open source software especially GNU Linux. my favorite distribution fedora silverblue. working on containerzation technologies like Docker and Kubernetes. kubeflow.        </p>
+        I’ve long been interested in software development and design and always enjoy learning about new and exciting technologies. my experience in Python Frameworks Django, Flask. Golang, Bash and JavaScript libraries like ReactJs. I love open source software especially GNU Linux, my favorite distribution fedora silverblue. working on containerzation technologies like Docker and Kubernetes. kubeflow.</p>
         <p>Also interested in machine learning, algorithms, linear algebra. statistics</p>
       </Details>
     </SkillsWrapper>
